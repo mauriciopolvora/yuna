8b647632483f8f2677c02f12a3a02611ab3cfeb4
Mauricio Polvora <mauricio.polvora@gmail.com>
2020-04-23 17:33:55 +0100

    Added commands
    + yuna boa noite
    + yuna bom dia

---
+++ src/main/java/me/yuna/commands/BoaNoite.java
@@ -0,0 +1,18 @@
+package me.yuna.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class BoaNoite extends Command {
+	
+	public BoaNoite() {
+	 super.name = "dorme";
+	}
+
+	@Override
+	protected void execute(CommandEvent event) {
+		
+		event.reply("Obrigado, boa noite dorme bem <3, sonhos cor de rosa abraçados ao teu peluche.");
+	}
+
+}
---
+++ src/main/java/me/yuna/commands/BomDia.java
@@ -0,0 +1,18 @@
+package me.yuna.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class BomDia extends Command {
+	
+	public BomDia() {
+	 super.name = "bom";
+	}
+
+	@Override
+	protected void execute(CommandEvent event) {
+		
+		event.reply("Obrigado, mas quem vai ter um bom dia és tu <3");
+	}
+
+}
