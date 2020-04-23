461749a27f7075b9810d5b365f3032dbe0224d68
XLP <1st@xlp-now.de>
2020-04-23 05:08:01 +0200

    Base implementation of youtube-dl

--- src/main/java/me/yuna/commands/music/NowPlayingCommand.java
+++ src/main/java/me/yuna/commands/music/NowPlayingCommand.java
@@ -5,12 +5,13 @@ import java.util.concurrent.TimeUnit;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
+import me.yuna.commands.music.PlayerManager.AudioInformation;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 public class NowPlayingCommand extends Command {
-	
+
 	public NowPlayingCommand() {
 		super.name = "np";
 	}
@@ -24,28 +25,40 @@ public class NowPlayingCommand extends Command {
 
 		if(player.getPlayingTrack() == null) {
 			event.reply("I am not playing annything currently");
+			return;
 		}
 		
-		AudioTrackInfo info = player.getPlayingTrack().getInfo();
+		AudioTrack track = player.getPlayingTrack();
 		
+		AudioInformation audioInfo = AudioInformation.fromTrack(track);
+
 		EmbedBuilder builder = new EmbedBuilder();
 			builder.setTitle("Currently Playing: ");
 			builder.setDescription(String.format(
 	                "**Playing** [%s](%s)\n%s %s - %s",
-	                info.title,
-	                info.uri,
+	                audioInfo.getTitle(),
+	                audioInfo.getUrl(),
 	                player.isPaused() ? "\u23F8" : "▶",
-	                formatTime(player.getPlayingTrack().getPosition()),
-	                formatTime(player.getPlayingTrack().getDuration())
+	                formatTimeMills(track.getPosition()),
+	                formatTimeSecs(audioInfo.getDuration())
 	        ));
 			
 		event.reply(builder.build());
 	}
 	
-	private String formatTime(long timeInMillis) {
+	private String formatTimeMills(long timeInMillis) {
         final long hours = timeInMillis / TimeUnit.HOURS.toMillis(1);
-        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1);
+        final long minutes = timeInMillis % TimeUnit.HOURS.toMillis(1) / TimeUnit.MINUTES.toMillis(1);
         final long seconds = timeInMillis % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+	}
+	
+	
+	private String formatTimeSecs(int timeInSecs) {
+        final long hours = timeInSecs / TimeUnit.HOURS.toSeconds(1);
+        final long minutes = timeInSecs % TimeUnit.HOURS.toSeconds(1) / TimeUnit.MINUTES.toSeconds(1);
+        final long seconds = timeInSecs % TimeUnit.MINUTES.toSeconds(1);
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
--- src/main/java/me/yuna/commands/music/PlayerManager.java
+++ src/main/java/me/yuna/commands/music/PlayerManager.java
@@ -1,5 +1,18 @@
 package me.yuna.commands.music;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+import com.sapher.youtubedl.YoutubeDL;
+import com.sapher.youtubedl.YoutubeDLException;
+import com.sapher.youtubedl.YoutubeDLRequest;
+import com.sapher.youtubedl.YoutubeDLResponse;
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
@@ -8,18 +21,24 @@ import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
+import org.json.JSONObject;
+
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class PlayerManager {
     private static PlayerManager INSTANCE;
     private final AudioPlayerManager playerManager;
     private final Map<Long, GuildMusicManager> musicManagers;
 
+    // Download-destination directory
+    public static final File downloadDirectory = new File("audio_cache");
+
     private PlayerManager() {
+
+        downloadDirectory.mkdirs();
+        downloadDirectory.mkdir();
+
         this.musicManagers = new HashMap<>();
 
         this.playerManager = new DefaultAudioPlayerManager();
@@ -27,8 +46,8 @@ public class PlayerManager {
         AudioSourceManagers.registerLocalSource(playerManager);
     }
 
-    public synchronized GuildMusicManager getGuildMusicManager(Guild guild) {
-        long guildId = guild.getIdLong();
+    public synchronized GuildMusicManager getGuildMusicManager(final Guild guild) {
+        final long guildId = guild.getIdLong();
         GuildMusicManager musicManager = musicManagers.get(guildId);
 
         if (musicManager == null) {
@@ -41,52 +60,148 @@ public class PlayerManager {
         return musicManager;
     }
 
-    public void loadAndPlay(TextChannel channel, String trackUrl , String index) {
-        GuildMusicManager musicManager = getGuildMusicManager(channel.getGuild());
-        
-        final int indexToInt = Integer.parseInt(index);
+    public void loadAndPlay(final TextChannel channel, final String trackUrl, final String index) {
+        final GuildMusicManager musicManager = getGuildMusicManager(channel.getGuild());
+        channel.sendMessage("Okay... *looking up*").queue();
+        try {
 
-        playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
-            private int index = indexToInt ;
+            // Build request
+            YoutubeDLRequest query = new YoutubeDLRequest(trackUrl);
+            query.setOption("quiet"); // --quet
+            query.setOption("ignore-errors"); // --ignore-errors
+            query.setOption("dump-json"); // --dump-json
+            query.setOption("retries", 10); // --retries 10
 
-			@Override
-            public void trackLoaded(AudioTrack track) {
-                channel.sendMessage("Adding to queue " + track.getInfo().title).queue();
+            // Make request and return response
+            YoutubeDLResponse queryResult;
+            queryResult = YoutubeDL.execute(query);
 
+            String[] dataBuffer = queryResult.getOut().split("\n");
+
+            for (String line : dataBuffer) {
+                channel.sendTyping();
+                //System.out.println("OUT: " + line);
+
+                try {
+
+                    JSONObject videoData = new JSONObject(line);
+                    final String videoUrl = videoData.getString("webpage_url");
+                    final String videoId = videoData.getString("id");
+                    final String videoTitle = videoData.getString("title");
+                    final String videoAuthor = videoData.getString("uploader");
+                    final int videoDuration = videoData.getInt("duration");
+                    String host = new URL(videoUrl).getHost();
+                    final String fileName = host + "_" + videoId + "_";
+                    // channel.sendMessage("FWD:: " + videoUrl + " - FN: " + fileName).queue();
+
+                    // Build request
+                    YoutubeDLRequest request = new YoutubeDLRequest(videoUrl, downloadDirectory.getAbsolutePath());
+                    request.setOption("ignore-errors"); // --ignore-errors
+                    request.setOption("extract-audio"); // --extract-audio
+                    request.setOption("restrict-filenames"); // --restrict-filenames
+
+                    // String sanatizedTitle = videoTitle.replace("\"", "'");
+
+                    request.setOption("add-metadata"); // --add-metadata
+
+                    request.setOption("output", fileName + "%(title)s.%(ext)s"); // --output www.youtube.com_tmbfT7-tZiM_%(title)s.%(ext)s
+                    request.setOption("retries", 10); // --retries 10
+
+                    // Make request and return response
+                    // YoutubeDLResponse response;
+
+                    channel.sendTyping();
+                    YoutubeDL.execute(request);
+                    channel.sendTyping();
+
+                    // File downloadedFile = new File(downloadDirectory, fileName);
+
+                    File[] foundFiles = downloadDirectory.listFiles(new FilenameFilter() {
+                        @Override
+                        public boolean accept(File file, String name) {
+                            return name.indexOf(fileName, 0) == 0;
+                        }
+                    });
+
+                    if (foundFiles.length <= 0) {
+                        throw new FileNotFoundException("File, beginning with \"" + fileName + ".\" not found");
+                    }
+
+                    File downloadedFile = foundFiles[0];
+
+                    if (!downloadedFile.exists()) {
+                        throw new FileNotFoundException("File \"" + downloadedFile.getAbsolutePath() + "\"not found");
+                    }
+
+                    System.out.println("DLF: " + downloadedFile.getAbsolutePath());
+                    playFromFile(downloadedFile.getAbsolutePath(), musicManager, channel, new AudioInformation(videoTitle, videoAuthor, videoUrl, videoDuration));
+                
+                
+                } catch (YoutubeDLException e) {
+                    e.printStackTrace(System.err);
+                    channel.sendMessage("Skipping item: Failed youtube download").queue();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace(System.err);
+                    channel.sendMessage("F. ... check my logs pls .-.\n   ~ Marlformed URLS  (Skipping item)").queue();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace(System.err);
+                    channel.sendMessage("F. ... check my logs pls .-.\n   ~ File not stored  (Skipping item)").queue();
+                } catch (Exception e) {
+                    e.printStackTrace(System.err);
+                    channel.sendMessage("F. ... check my logs pls .-.\n   ~ General Exception  (Skipping item)").queue();
+                }
+            }
+
+            if (dataBuffer.length > 1) {
+                channel.sendMessage("Successful added " + dataBuffer.length + " songs to queue").queue();
+            }
+
+        } catch (YoutubeDLException e) {
+            e.printStackTrace(System.err);
+            channel.sendMessage("F. ... check my logs pls .-.\n   ~ Failed youtube download").queue();
+        }
+
+    }
+
+    private void playFromFile(String file, GuildMusicManager musicManager, TextChannel channel, AudioInformation audioInfo) {
+
+        Future<Void> future = playerManager.loadItem(file, new AudioLoadResultHandler() {
+
+            @Override
+            public void trackLoaded(final AudioTrack track) {
+                //AliasedAudioTrack aliasedTrack = new AliasedAudioTrack(track, title, author);
+                track.setUserData(audioInfo);
+                AudioInformation audioInfo = AudioInformation.fromTrack(track);
+                channel.sendMessage("Adding to queue " + audioInfo.getTitle()).queue();
                 play(musicManager, track);
             }
 
             @Override
-            public void playlistLoaded(AudioPlaylist playlist) {
-                AudioTrack firstTrack = playlist.getSelectedTrack();
-
-                if (firstTrack == null) {
-                    firstTrack = playlist.getTracks().remove(0);
-                }
-
-                channel.sendMessage("Adding to queue: " + firstTrack.getInfo().title + " (first track of playlist " + playlist.getName() + ")").queue();
-                
-                for (int i = (this.index - 1) ; i < playlist.getTracks().size(); i++) {
-                	
-					play(musicManager, playlist.getTracks().get(i));
-				}
-          
+            public void playlistLoaded(final AudioPlaylist playlist) {
+                channel.sendMessage("~ Playlist? .. heh .-.").queue();
             }
 
             @Override
             public void noMatches() {
-                channel.sendMessage("Nothing found by " + trackUrl).queue();
+                channel.sendMessage("~ Donwload not found? .-.").queue();
             }
 
             @Override
-            public void loadFailed(FriendlyException exception) {
+            public void loadFailed(final FriendlyException exception) {
                 channel.sendMessage("Could not play: " + exception.getMessage()).queue();
             }
         });
 
+        while (!future.isDone()) {
+            try {
+                channel.sendTyping();
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 
-    private void play(GuildMusicManager musicManager, AudioTrack track) {
+    private void play(final GuildMusicManager musicManager, final AudioTrack track) {
         musicManager.scheduler.queue(track);
     }
 
@@ -97,4 +212,55 @@ public class PlayerManager {
 
         return INSTANCE;
     }
+
+    public static class AudioInformation {
+        private String title;
+        private String author;
+        private String url;
+        private int duration;
+
+        public AudioInformation(String title, String author, String url, int duration) {
+            this.title = title;
+            this.author = author;
+            this.url = url;
+            this.duration = duration;
+        }
+
+        /**
+         * @return the author
+         */
+        public String getAuthor() {
+            return author;
+        }
+
+        /**
+         * @return the duration
+         */
+        public int getDuration() {
+            return duration;
+        }
+
+        /**
+         * @return the title
+         */
+        public String getTitle() {
+            return title;
+        }
+
+        /**
+         * @return the url
+         */
+        public String getUrl() {
+            return url;
+        }
+
+        public static AudioInformation fromTrack(AudioTrack track) {
+            Object userData = track.getUserData();
+            if (userData instanceof AudioInformation) {
+                return (AudioInformation) userData;
+            } else {
+                throw new ClassCastException("User-data in AudioTrack is not of the type AudioInformation!");
+            }
+        }
+    } 
 }
